0745b70984dca957ae0973c6c0c0b5fb34d485b3
goatsfish <goatsfish@gmail.com>
2023-07-19 23:27:01 +0900

    test: 인수테스트 @Disable

--- backend/baton/src/test/java/touch/baton/assure/runnerpost/RunnerPostAssuredReadTest.java
+++ backend/baton/src/test/java/touch/baton/assure/runnerpost/RunnerPostAssuredReadTest.java
@@ -1,6 +1,5 @@
 package touch.baton.assure.runnerpost;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import touch.baton.assure.fixture.MemberFixture;
 import touch.baton.assure.fixture.RunnerFixture;
@@ -18,7 +17,6 @@ import java.util.ArrayList;
 
 class RunnerPostAssuredReadTest extends AssuredTestConfig {
 
-    @Disabled
     @Test
     void 러너의_게시글_식별자값으로_러너_게시글_상세_정보_조회에_성공한다() {
         final Member member = MemberFixture.from("헤나",
